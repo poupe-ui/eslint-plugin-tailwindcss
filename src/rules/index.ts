4faf5acf93e1ca2cfd5120dff51b6506cd510509
Alejandro Mery <amery@apptly.co>
2025-06-29 13:42:51 +0000

    feat(rules): add rules index export file
    Export all ESLint rules from a central index for plugin registration.
    
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

---
+++ src/rules/index.ts
@@ -0,0 +1,6 @@
+export { validThemeFunction } from './valid-theme-function';
+export { noArbitraryValueOveruse } from './no-arbitrary-value-overuse';
+export { preferThemeTokens } from './prefer-theme-tokens';
+export { validModifierSyntax } from './valid-modifier-syntax';
+export { noConflictingUtilities } from './no-conflicting-utilities';
+export { validApplyDirective } from './valid-apply-directive';
